95635a4d7213925aaacdafe0329ebcc7adb85499
Glitch (teenee-url-fcc) <none>
2018-01-09 23:42:01 +0000

    😂🐙 Checkpoint server.js:565149/105

--- server.js
+++ server.js
@@ -65,18 +65,18 @@ app.get('/:shortURL', function(req,res){
   });
 });
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
+// // could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
+// app.post("/dreams", function (request, response) {
+//   dreams.push(request.query.dream);
+//   response.sendStatus(200);
+// });
 
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+// // Simple in-memory store for now
+// var dreams = [
+//   "Find and count some sheep",
+//   "Climb a really tall mountain",
+//   "Wash the dishes"
+// ];
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
