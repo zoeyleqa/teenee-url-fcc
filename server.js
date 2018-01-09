b980840cb6771bee9ea33d09c3453b61c40dc3c8
Glitch (teenee-url-fcc) <none>
2018-01-09 21:05:56 +0000

    🐔😄 Checkpoint server.js:565149/274

--- server.js
+++ server.js
@@ -20,8 +20,12 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
+app.get('/new/:url', function(req, res){
+  var url = req.params.url;
+  
+  mongo.
+  
+  
 });
 
 // could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
