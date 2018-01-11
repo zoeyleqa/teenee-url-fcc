f19d92c43dfa539f26fcf7c82fcc811d89666297
Glitch (teenee-url-fcc) <none>
2018-01-11 11:43:52 +0000

    🐋🔫 Checkpoint server.js:565149/388

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/new/*', function(req, res){
   if(validURL.isUri(url)){
     
     var entry = { "original" : url, 
-                  "shortened_url" : req.protocol + '://' + req.headers.host + '/new/' + id.generate()}; 
+                  "shortened_url" : req.protocol + '://' + req.hostname +'/' + id.generate()}; 
 console.log(JSON.stringify(entry));
   mongo.connect(dburl, function(err, db){
     if(err) throw err;
@@ -56,13 +56,13 @@ app.get('/:shortURL', function(req,res){
     console.log(req.params.shortURL);
     // var db = client.db(process.env.DBNAME);
     var collection = db.collection(process.env.COLLECTION);
-    collection.find({ "shortened_url" : req.protocol + '://' + req.headers.host + req.params.shortURL },//has extra /new/
-                    { "shortened_url" : 0}
+    collection.find({ "shortened_url" : req.protocol + '://' + req.hostname + '/' + req.params.shortURL },
+                    { "_id": 0,"shortened_url" : 0}
                    ).toArray(function(err2, doc){
                   if(err2) throw err2;
       
                   // console.log(doc[0]);
-                  res.redirect(doc[0]);
+                  res.redirect(doc[0].original);
     });
     db.close();
   });
