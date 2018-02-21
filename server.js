8e6287e0fcf736f8fc90d66b2e0c4f787e695a16
Glitch (teenee-url-fcc) <none>
2018-02-21 03:04:52 +0000

    ☕️🏰 Updated with Glitch

--- server.js
+++ server.js
@@ -13,13 +13,10 @@ var dburl = process.env.DBPROGRAM + '://' + process.env.USER +':'+ process.env.P
 + '@'+process.env.HOST + ':' + process.env.DBPORT+'/' +process.env.DBNAME ;
 // var dburl = `mongodb://${encodeURIComponent(process.env.USER)}:${encodeURIComponent(process.env.PASS)}@${encodeURIComponent(process.env.HOST)}:${encodeURIComponent(process.env.DBPORT)}/${encodeURIComponent(process.env.DBNAME)}`
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-// http://expressjs.com/en/starter/static-files.html
+
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
@@ -57,32 +54,18 @@ app.get('/:shortURL', function(req,res){
     mongo.connect(dburl, function(err, db){
     if(err) throw err;
     // console.log(req.params.shortURL);
-    // var db = client.db(process.env.DBNAME);
     var collection = db.collection(process.env.COLLECTION);
     collection.find({ "short_url" : req.protocol + '://' + req.hostname + '/' + req.params.shortURL },
                     { "_id": 0,"short_url" : 0} //exclusion
                    ).toArray(function(err2, doc){
                   if(err2) throw err2;
       
-                  // doc[0] for getting the first find or only find
+
                   res.redirect(doc[0].original_url);
     });
     db.close();
   });
 });
-
-// // could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-// app.post("/dreams", function (request, response) {
-//   dreams.push(request.query.dream);
-//   response.sendStatus(200);
-// });
-
-// // Simple in-memory store for now
-// var dreams = [
-//   "Find and count some sheep",
-//   "Climb a really tall mountain",
-//   "Wash the dishes"
-// ];
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
